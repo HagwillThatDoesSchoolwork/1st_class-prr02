b6ad2409d86968123470e47508036763b00547ab
William Hagström <william.hagstrom@elev.ga.lbs.se>
2018-08-29 14:06:26 +0200

    Really Important Program
    What it says on the tin really.

---
+++ WAOW.bat
@@ -0,0 +1,24 @@
+@echo off
+cls
+
+:start
+set/p %answer%=Am I the best? 
+	if %ANSWER%==Yes goto correct
+	if %ANSWER%==yes goto correct
+	if %ANSWER%==No goto wrong
+	if %ANSWER%==no goto wrong
+echo Try again please.
+
+
+:correct
+color 02
+echo I'm glad we agree.
+pause
+goto end
+
+:wrong
+color 0C
+echo how could you be so wrong?
+pause
+
+:end
